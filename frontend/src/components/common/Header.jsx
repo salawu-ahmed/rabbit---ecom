f5b7b750de4891ab192a4f9e461b5ae56e69982a
ahmed <a.salawu@outlook.com>
2025-02-21 05:26:37 +0000

    created the navbar component

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Topbar from '../layout/Topbar'
+import Navbar from './Navbar'
 
 export default function Header() {
   return (
     <div>
       {/* top bar */}
       <Topbar />
+      {/* Navbar */}
+      <Navbar />
     </div>
   )
 }
---
+++ frontend/src/components/common/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Link } from 'react-router'
+import { HiOutlineUser, HiOutlineShoppingBag, HiMenu } from 'react-icons/hi'
+
+export default function Navbar() {
+    return (
+        <>
+            <nav className="container flex items-center justify-between py-4 px-6">
+                {/* logo - text */}
+                <div>
+                    <Link to='/' className='text-2xl font-medium'>Rabbit</Link>
+                </div>
+                {/* center */}
+                <div className="hidden md:flex space-x-6">
+                    <Link to='#' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>for men</Link>
+                </div>
+                <div className="hidden md:flex space-x-6">
+                    <Link to='#' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>for women</Link>
+                </div>
+                <div className="hidden md:flex space-x-6">
+                    <Link to='#' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>top wear</Link>
+                </div>
+                <div className="hidden md:flex space-x-6">
+                    <Link to='#' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>bottom wear</Link>
+                </div>
+                {/* right */}
+                <div className="flex items-center space-x-4">
+                    <Link className='hover:text-black'>
+                        <HiOutlineUser size={25} className='text-gray-700' />
+                    </Link>
+                    <button>
+                        <HiOutlineShoppingBag size={25}/>
+                        <span className='absolute bg-rabbit-red -top-1 text-white text-xs rounded px-2 py-0.5'>4</span>
+                    </button>
+
+                    {/* Search componnet */}
+
+                    <button className='md:hidden'>
+                        <HiMenu size={25} className='text-gray-700'/>
+                    </button>
+                </div>
+            </nav>
+        </>
+    )
+}
