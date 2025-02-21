fc645a76b6d17f875e2135438303e6714cce6902
ahmed <a.salawu@outlook.com>
2025-02-21 09:32:54 +0000

    created mobile nav menu

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -3,47 +3,43 @@ import { Link } from 'react-router'
 import { HiOutlineUser, HiOutlineShoppingBag, HiMenu } from 'react-icons/hi'
 import SearchBar from './SearchBar'
 import CartDrawer from './CartDrawer'
+import { IoMdClose } from 'react-icons/io'
 
 export default function Navbar() {
     // refs
     const cartDrawerRef = useRef()
+    const mobileNavRef = useRef()
 
     // states
     const [isCartDrawerOpen, setIsCartDrawerOpen] = useState(false)
     const [isMobileNavOpen, setIsMobileNavOpen] = useState(false)
 
-    const handleClickOutsideCart = useCallback(function handleClickOutsideCart(e) {
-        if(isCartDrawerOpen && !cartDrawerRef.current.contains(e.target)){
+    const handleClickOutsideElements = useCallback(function handleClickOutsideElements(e) {
+        if (isMobileNavOpen && !mobileNavRef.current.contains(e.target)) {
+            setIsMobileNavOpen(false)
+        }
+        if (isCartDrawerOpen && !cartDrawerRef.current.contains(e.target)) {
             setIsCartDrawerOpen(false)
         }
-        console.log('You clicked me');    
-    }, [isCartDrawerOpen])
+    }, [isCartDrawerOpen, isMobileNavOpen])
+
 
     useEffect(() => {
-        document.addEventListener('mousedown', handleClickOutsideCart)
+        document.addEventListener('mousedown', handleClickOutsideElements)
         return () => {
-            document.removeEventListener('mousedown', handleClickOutsideCart)
+            document.removeEventListener('mousedown', handleClickOutsideElements)
         }
-    }, [handleClickOutsideCart])
+    }, [handleClickOutsideElements])
 
 
-    function toggleCartDrawer () {
+    function toggleCartDrawer() {
         setIsCartDrawerOpen(prev => !prev)
     }
-    function toggleMobileNav () {
+    function toggleMobileNav() {
         setIsMobileNavOpen(prev => !prev)
     }
 
-    // function handleClickOutsideCart (e) {
-    //     if(isCartDrawerOpen && !cartDrawerRef.current.contains(e.target)){
-    //         setIsCartDrawerOpen(false)
-    //     }
-    //     console.log('You clicked me');  
-    // } 
-  
 
-
-   
     return (
         <>
             <nav className="container flex items-center justify-between py-4 px-6">
@@ -75,9 +71,9 @@ export default function Navbar() {
                     <Link className='hover:text-black'>
                         <HiOutlineUser size={25} className='text-gray-700' />
                     </Link>
-                    <button 
-                    className='relative'
-                    onClick={toggleCartDrawer}
+                    <button
+                        className='relative'
+                        onClick={toggleCartDrawer}
                     >
                         <HiOutlineShoppingBag size={25} />
                         <span className='absolute bg-rabbit-red -top-1 text-white text-xs rounded-full px-2 py-0.5'>4</span>
@@ -88,14 +84,38 @@ export default function Navbar() {
                         <SearchBar />
                     </div>
 
-                    <button className='md:hidden'>
+                    <button className='md:hidden' onClick={toggleMobileNav}>
                         <HiMenu size={25} className='text-gray-700' />
                     </button>
                 </div>
             </nav>
-            <CartDrawer onClose={toggleCartDrawer} isOpen={isCartDrawerOpen} ref={cartDrawerRef}/>
+            <CartDrawer onClose={toggleCartDrawer} isOpen={isCartDrawerOpen} ref={cartDrawerRef} />
 
-            
+
+            {/* Mobile Navigation */}
+            <div
+                ref={mobileNavRef}
+                className={
+                    `h-full w-3/4 bg-white fixed top-0 right-0 shadow-lg transform transition-transform duration-300 sm:hidden z-50
+                ${isMobileNavOpen ? 'translate-x-0' : 'translate-x-full'}`
+                }>
+                <div className="flex justify-end p-4">
+                    <IoMdClose
+                        size={25}
+                        className='text-gray-300 hover:text-gray-600 cursor-pointer'
+                        onClick={toggleMobileNav}
+                    />
+                </div>
+                <div className="p-4">
+                    <h2 className="mb-4 font-bold fontxl">Menu</h2>
+                    <nav className="space-y-4">
+                        <Link to='#' onClick={toggleMobileNav} className='text-gray-600 cursor-pointer block hover:text-black text-sm font-medium uppercase'>for men</Link>
+                        <Link to='#' onClick={toggleMobileNav} className='text-gray-600 cursor-pointer block hover:text-black text-sm font-medium uppercase'>for women</Link>
+                        <Link to='#' onClick={toggleMobileNav} className='text-gray-600 cursor-pointer block hover:text-black text-sm font-medium uppercase'>top wear</Link>
+                        <Link to='#' onClick={toggleMobileNav} className='text-gray-600 cursor-pointer block hover:text-black text-sm font-medium uppercase'>bottom wear</Link>
+                    </nav>
+                </div>
+            </div>
         </>
     )
 }
