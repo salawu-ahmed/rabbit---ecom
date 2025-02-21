9046319b9d65f3490f5282dd31b39e7e1b7fb3e4
ahmed <a.salawu@outlook.com>
2025-02-21 19:42:22 +0000

    added images

--- frontend/src/components/layout/UserLayout.jsx
+++ frontend/src/components/layout/UserLayout.jsx
@@ -6,6 +6,9 @@ export default function UserLayout() {
   return (
     <div>
       <Header />
+      <main className='w-full h-screen'>
+        <h2 className="text-5xl">This is the main section of our website </h2>
+      </main>
       <Footer />
     </div>
   )
