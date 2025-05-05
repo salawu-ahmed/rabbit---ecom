62fb5f61fe8d994514aa0c8d5d0cd4324cfea7f8
ahmed <a.salawu@outlook.com>
2025-05-05 12:18:32 +0100

    initail bckend setup

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+dotenv.config()
+
+// database connection
+connectDB()
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Rabbit App')
+})
+
+app.listen(process.env.port, () => console.log(`Server running on http://localhost:${process.env.port}`)
+)
