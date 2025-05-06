823711e932a3c6239245f49247e2a002421bc2a9
ahmed <a.salawu@outlook.com>
2025-05-06 05:24:00 +0100

    created update product route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const Product = require('../models/Product')
-const protect = require('../middleware/authMiddleware')
+const { protect, admin } = require('../middleware/authMiddleware')
 
 const router = express.Router()
 
 // @route POST /api/products
 // @desc Create a new product
 // access private/Admin
-router.post('/', protect, async function (req, res) {
+router.post('/', protect, admin, async (req, res) => {
     try {
         const {
             name,
             description,
-            price:
+            price,
             discountPrice,
             countInStock,
             category,
@@ -34,7 +34,7 @@ router.post('/', protect, async function (req, res) {
         const product = new Product({
             name,
             description,
-            price:
+            price,
             discountPrice,
             countInStock,
             category,
@@ -61,5 +61,70 @@ router.post('/', protect, async function (req, res) {
         res.status(500).send('Server Error')
     }
 }) 
+
+//@route PUT api/products/:id
+//@desc update an existing product using its id 
+//@access Pivat/Admin
+router.put('/:id', protect, admin, async (req, res) => {
+    try {
+        const {
+            name,
+            description,
+            price,
+            discountPrice,
+            countInStock,
+            category,
+            brand,
+            material,
+            sizes,
+            colors,
+            collections,
+            gender,
+            images,
+            isFeatured,
+            isPublished,
+            dimensions,
+            tags,
+            weight,
+            sku 
+        } = req.body
+
+        let product = await Product.findById(req.params.id)
+
+        if(product) {
+            product.name = name || product.name
+            product.description = description || product.description
+            product.price = price || product.price
+            product.discountPrice = discountPrice || product.discountPrice
+            product.countInStock = countInStock || product.countInStock
+            product.category = category || product.category
+            product.brand = brand || product.brand
+            product.material = material || product.material
+            product.sizes = sizes || product.sizes
+            product.colors = colors || product.colors
+            product.collections = collections || product.collections
+            product.gender = gender || product.gender
+            product.images = images || product.images
+            product.isFeatured = isFeatured !== undefined ? isFeatured : product.isFeatured
+            product.isPublished = isPublished !== undefined ? isPublished : product.isPublished
+            product.dimensions = dimensions || product.dimensions
+            product.sku = sku || product.sku
+            product.tags = tags || product.tags
+            product.weight = weight || product.weight
+
+            const updatedProduct = await product.save()
+            res.json(updatedProduct)
+        }else {
+            res.status(404).json({
+                message: 'Product not found'
+            })
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error')
+    }
+})
+
+
 
 module.exports = router
