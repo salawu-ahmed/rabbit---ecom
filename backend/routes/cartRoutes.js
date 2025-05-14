5ed5f4d0648b38fd89e54ef0b0b3b88571d6495d
ahmed <a.salawu@outlook.com>
2025-05-14 18:51:33 +0100

    created route to delete product from cart

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -144,4 +144,41 @@ router.put('/', async function (req, res) {
     }
 })
 
+
+// @route DELETE /api/cart
+// @desc remove a product from the cart 
+// @access Public 
+router.delete('/', async function (req, res) {
+    const { productId, size, color, quantity, userId, guestId } = req.body
+    try {
+        let cart = await getCart(userId, guestId)
+        if (!cart) {
+            return res.status(404).json({ message: 'Cart not found' })
+        }
+
+        const productIndex = cart.products.findIndex(
+            (p) => p.productId.toString() === productId &&
+                p.color === color &&
+                p.size === size
+        )
+
+        if(productIndex > -1) {
+            cart.products.splice(productIndex, 1)
+            cart.totalPrice = cart.products.reduce(
+                (acc, item) => acc + item.price * item.quantity,
+                0
+            )
+            await cart.save()
+            return res.status(200).json(cart)
+        } else {
+            return res.status(404).json({
+                message: 'Product not found in cart'
+            })
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({message: 'Server Error'})
+        
+    }
+})
 module.exports = router
