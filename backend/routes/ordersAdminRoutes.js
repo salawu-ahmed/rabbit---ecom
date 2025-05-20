dc66d15abd63266ee2d1620de3e6aae66aef7d77
ahmed <a.salawu@outlook.com>
2025-05-20 14:35:27 +0100

    added /api/admin/orders/:id - update an order status

--- backend/routes/ordersAdminRoutes.js
+++ backend/routes/ordersAdminRoutes.js
@@ -17,4 +17,27 @@ router.get('/', protect, admin, async function(req, res) {
     }
 })
 
+
+// /api/admin/orders/:id
+router.put('/:id', protect, admin, async function(req, res){
+    const { id } = req.params
+    const { status } = req.body
+    try {
+        const order = await Order.findById(id)
+        if(order) {
+            order.status = status || order.status
+            order.isDelivered = status === "Delivered" ? true : order.isDelivered
+            order.deliveredAt = status === "Delivered" ? Date.now() : order.deliveredAt
+
+            const updatedOrder = await order.save()
+            res.json(updatedOrder)
+        } else {
+            res.status(404).json({ message: 'Order not found'})
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error'})
+    }
+})
+
 module.exports = router
