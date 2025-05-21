81f67c5dd969017e343e6ef7e33c9de434bc1c9b
ahmed <a.salawu@outlook.com>
2025-05-21 09:53:57 +0100

    added redux store and authSlices

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,38 +19,43 @@ import ProductManagement from './components/admin/ProductManagement'
 import EditProductPage from './components/admin/EditProductPage'
 import OrderManagement from './components/admin/OrderManagement'
 
+import { Provider } from 'react-redux'
+import store from './redux/store'
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <Toaster position='top-right' />
-      {/* 
+    <Provider store={store}>
+      <BrowserRouter>
+        <Toaster position='top-right' />
+        {/* 
     www.rabbit.com/home  
     www.rabbit.com/products  
     www.rabbit.com/cart
     */}
-      <Routes>
-        <Route path='/' element={<UserLayout />}>
-          <Route index element={<Home />} />
-          <Route path='login' element={<Login />} />
-          <Route path='register' element={<Register />} />
-          <Route path='profile' element={<Profile />} />
-          <Route path='collections/:collection' element={<CollectionPage />} />
-          <Route path='product/:id' element={<ProductDetails />} />
-          <Route path='checkout' element={<Checkout />} />
-          <Route path='order-confirmation' element={<OrderConfirmationPage />} />
-          <Route path='order/:id' element={<OrderDetailsPage />} />
-          <Route path='my-orders' element={<MyOrdersPage />} />
-        </Route>
-        {/* User Routes */}
-        {/* Admin Routes */}
-        <Route path='/admin' element={<AdminLayout />}>
-          <Route index element={<AdminHomePage/>}/>
-          <Route path='users' element={<UserMangement/>} />
-          <Route path='products' element={<ProductManagement/>} />
-          <Route path='products/:id/edit' element={<EditProductPage/>} />
-          <Route path='orders' element={<OrderManagement/>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+        <Routes>
+          <Route path='/' element={<UserLayout />}>
+            <Route index element={<Home />} />
+            <Route path='login' element={<Login />} />
+            <Route path='register' element={<Register />} />
+            <Route path='profile' element={<Profile />} />
+            <Route path='collections/:collection' element={<CollectionPage />} />
+            <Route path='product/:id' element={<ProductDetails />} />
+            <Route path='checkout' element={<Checkout />} />
+            <Route path='order-confirmation' element={<OrderConfirmationPage />} />
+            <Route path='order/:id' element={<OrderDetailsPage />} />
+            <Route path='my-orders' element={<MyOrdersPage />} />
+          </Route>
+          {/* User Routes */}
+          {/* Admin Routes */}
+          <Route path='/admin' element={<AdminLayout />}>
+            <Route index element={<AdminHomePage />} />
+            <Route path='users' element={<UserMangement />} />
+            <Route path='products' element={<ProductManagement />} />
+            <Route path='products/:id/edit' element={<EditProductPage />} />
+            <Route path='orders' element={<OrderManagement />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   )
 }
---
+++ frontend/src/redux/slices/authSlice.js
@@ -0,0 +1,99 @@
+import { createSlice, createAsyncThunk, isRejectedWithValue } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+// retrieve user info from localstorage if it is available
+const userFromStorage = localStorage.getItem('userInfo') ?
+    JSON.parse(localStorage.getItem('userInfo')) : null
+
+// check for an existing guestID in the localStorage or generate one
+const initialGuestID = localStorage.getItem('guestID') || `guest_${new Date().getTime()}`
+localStorage.setItem('guestID', initialGuestID)
+
+
+// initialState
+const initialState = {
+    user: userFromStorage,
+    guestId: initialGuestID,
+    loading: false,
+    error: null
+}
+
+// Async Thunk for User Login
+export const loginUser = createAsyncThunk('auth/loginUser', async (userData, { rejectWithValue }) => {
+    try {
+        const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/users/login`, userData)
+        localStorage.setItem('userInfo', JSON.stringify(response.data.user))
+        localStorage.setItem('userToken', response.data.token)
+
+        // return the user 
+        return response.data.user
+    } catch (error) {
+        return rejectWithValue(error.response.data)
+    }
+})
+
+
+// Async Thunk for User Registration
+export const registerUser = createAsyncThunk('auth/registerUser', async (userData, { rejectWithValue }) => {
+    try {
+        const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/users/register`, userData)
+        localStorage.setItem('userInfo', JSON.stringify(response.data.user))
+        localStorage.setItem('userToken', response.data.token)
+
+        // return the user 
+        return response.data.user
+    } catch (error) {
+        return rejectWithValue(error.response.data)
+    }
+})
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        logout: (state) => {
+            state.user = null
+            state.guestId = `guest_${new Date().getTime()}`
+            localStorage.removeItem('userInfo')
+            localStorage.removeItem('userToken')
+            localStorage.setItem('guestId', state.guestId)
+        },
+        generatenNewGuestId: (state) => {
+            state.guestId = `guest_${new DataView().getTime()}`
+            localStorage.setItem('guestId', state.guestId)
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(loginUser.pending, (state) => {
+                state.loading = true
+                state.error = null
+            })
+            .addCase(loginUser.fulfilled, (state, action) => {
+                state.loading = false
+                state.error = action.payload
+            })
+            .addCase(loginUser.rejected, (state, action) => {
+                state.loading = true
+                state.error = action.payload.message
+            })
+
+            // register user
+            .addCase(registerUser.pending, (state) => {
+                state.loading = true
+                state.error = null
+            })
+            .addCase(registerUser.fulfilled, (state, action) => {
+                state.loading = false
+                state.error = action.payload
+            })
+            .addCase(registerUser.rejected, (state, action) => {
+                state.loading = true
+                state.error = action.payload.message
+            })
+    }
+})
+
+
+export const { logout, generatenNewGuestId } = authSlice.actions
+export default authSlice.reducers
